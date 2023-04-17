e89dc41cfc8632f2bc3b6396a89162ce9706bbb0
SupremePrimeRick <alfredfredrick41@gmail.com>
2023-04-17 16:17:03 -0500

    done

--- 0x00-ES6_basic/0-main.js
+++ 0x00-ES6_basic/0-main.js
@@ -1,5 +1,3 @@
-import getNeighborhoodsList from './2-arrow.js';
+import appendToEachArrayValue from './10-loops.js';
 
-const neighborhoodsList = new getNeighborhoodsList();
-const res = neighborhoodsList.addNeighborhood('Noe Valley');
-console.log(res);
+console.log(appendToEachArrayValue(['appended', 'fixed', 'displayed'], 'correctly-'));
