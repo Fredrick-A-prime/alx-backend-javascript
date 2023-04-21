8313df29166bafa087b7bbaefad866dbf63548ba
SupremePrimeRick <alfredfredrick41@gmail.com>
2023-04-21 18:30:38 -0500

    more tasks resolved

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,11 +1,17 @@
 export default class Building {
-	constructor(sqft = 0) {
-		if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
-			throw new Error('Class extending Building must override evacuationWarningMessage');
-		}
-		this._sqft = sqft;
-	}
-	get sqft() {
-		return this._sqft;
-	}
+  constructor(sqft) {
+    if (
+      new.target !== Building
+      && typeof this.evacuationWarningMessage !== 'function'
+    ) {
+      throw new Error(
+        'Class extending Building must override evacuationWarningMessage',
+      );
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
 }
